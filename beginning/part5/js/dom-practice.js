09ecd680627718ba8128b2b23a14a6bfae47de2e
badimala218 <badimala@ucsd.edu>
2024-10-17 13:35:52 -0700

    Implement part 5
    Implemented part 5 of the assignment.

--- beginning/part5/js/dom-practice.js
+++ beginning/part5/js/dom-practice.js
@@ -1,12 +1,36 @@
 /*eslint-env browser*/
 
 //STEP 1
+const buttonClick = function(e)
+{
+    window.alert("I have been clicked");
+};
 
 //STEP 2
+button2.onclick = function(e) 
+{
+    window.alert("I have been clicked");
+};
 
 //STEP 3
+button3.addEventListener("click",
+    buttonClick);
 
 //STEP 4
+button4.addEventListener("click",
+    function(e) {
+        window.alert("I have been clicked");
+    }
+);
 
 //STEP 5
+document.addEventListener("DOMContentLoaded", () =>
+{
+    document.getElementById("button5").addEventListener("click",
+        function(e) {
+            window.alert("I have been clicked");
+        }
+    );
+});
+
 
